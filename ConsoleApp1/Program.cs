993cf730c11c936df7da8e833e132a9575256d8d
Jeslin-C-John <111417450+Jeslin-C-John@users.noreply.github.com>
2022-11-21 18:59:25 +0530

    Day 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,58 @@
-﻿class Program
+﻿using System.Data.SQLite;
+
+class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!OK");
+        //Console.WriteLine("Hello, World!OK");
+
+        //string cs = "Data Source=:memory:";
+
+        string cs = "Data Source=./Tasks.db";
+
+        //string stm = "Data Source=:memory:";
+
+        using var con = new SQLiteConnection(cs);
+        con.Open();
+
+        //using var cmd = new SQLiteCommand(stm, con);
+        using var cmd = new SQLiteCommand(con);
+
+        //string version = cmd.ExecuteScalar().ToString();
+        //Console.WriteLine($"SQLite version: {version}");
+
+
+        cmd.CommandText = "DROP TABLE IF EXISTS employees";
+        cmd.ExecuteNonQuery();
+
+        cmd.CommandText = @"CREATE TABLE employees(id INTEGER PRIMARY KEY,
+            name TEXT, hours INT)";
+        cmd.ExecuteNonQuery();
+
+        cmd.CommandText = "INSERT INTO employees(name, hours) VALUES('Tester',4)";
+        cmd.ExecuteNonQuery();
+
+        cmd.CommandText = "INSERT INTO employees(name, hours) VALUES('ios',8)";
+        cmd.ExecuteNonQuery();
+
+        cmd.CommandText = "INSERT INTO employees(name, hours) VALUES('Tester',4)";
+        cmd.ExecuteNonQuery();
+
+        cmd.CommandText = "INSERT INTO employees(name, hours) VALUES('Tester',4)";
+        cmd.ExecuteNonQuery();
+
+        cmd.CommandText = "INSERT INTO employees(name, hours) VALUES('Tester',4)";
+        cmd.ExecuteNonQuery();
+
+        cmd.CommandText = "INSERT INTO employees(name, hours) VALUES('Tester',4)";
+        cmd.ExecuteNonQuery();
+
+        cmd.CommandText = "INSERT INTO employees(name, hours) VALUES('Tester',4)";
+        cmd.ExecuteNonQuery();
+
+
+        Console.WriteLine("Table cars created");
+
+
     }
 }
